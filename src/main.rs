03d95cff229eb6435456533c44cc188fb9868981
andrew@tainan <dimsplendid@gmail.com>
2024-06-17 02:02:25 +0800

    Errorhandle finished

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,15 @@ use leptos::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount_to_body(|| view! { <App/> })
+    // leptos::mount_to_body(|| view! { <App/> })
+    leptos::mount_to_body(App)
 }
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let double_count = move || count.get() * 2;
-
     view! {
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
-
-            // class:red=move || count.get() % 2 == 1
-            // style="position: absolute"
-            // style:left=move || format!("{}px", count.get() + 100)
-            // style:background-color=move || format!("rgb({},{},100)", count.get(), 100)
-            style:max-width="400px"
-        >
-            // style=("--columns", count.get())
-            "Click me: "
-            {move || count.get()}
-        </button>
-        <br/>
-        // <progress
-        // max="50"
-        // value= move ||count.get()
-        // />
-        // <progress
-        // max="50"
-        // value= move ||count.get() * 2
-        // />
-        <ProgressBar1 max=16 progress=move || count.get()/>
-        <hr/>
-        <ProgressBar2 progress=double_count/>
-        <br/>
-        <ProgressBar2 max=32 progress=count/>
         <div>
-            <IterStatic/>
-        </div>
-        <div>
-            <StaticList/>
-        </div>
-        <div>
-            <DynamicList length=5/>
-        </div>
-        <div>
-            <p>Complex List</p>
-            <ComplexList/>
+            <ErrorHandleNumericInput/>
         </div>
     }
 }
@@ -215,12 +174,14 @@ fn DynamicList(
     }
 }
 
+/// test data base entry
 #[derive(Debug, Clone)]
 struct DatabaseEntry {
     key: String,
     value: i32,
 }
 
+/// need modified to truly reactive
 #[component]
 fn ComplexList() -> impl IntoView {
     // start with as set of three rows
@@ -236,28 +197,20 @@ fn ComplexList() -> impl IntoView {
         DatabaseEntry {
             key: "baz".to_string(),
             value: 30,
-        }
+        },
     ]);
     view! {
-        // when we click, update each row,
-        // doubling its value
         <button on:click=move |_| {
-            set_data.update(|data| {
-                for row in data {
-                    row.value *= 2;
-                }
-            });
-            // log the new value of the signal
+            set_data
+                .update(|data| {
+                    for row in data {
+                        row.value *= 2;
+                    }
+                });
             logging::log!("{:?}", data.get());
-        }>
-            "Update Values"
-        </button>
+        }>"Update Values"</button>
         // iterate over the rows and display each value
-        <For
-            each=move || data.get()
-            key=|state| state.key.clone()
-            let:child
-        >
+        <For each=move || data.get() key=|state| state.key.clone() let:child>
             <p>{child.value}</p>
         </For>
     }
@@ -267,3 +220,253 @@ fn ComplexList() -> impl IntoView {
 
 // Controlled Inputs
 // Framework controls the state of the input element
+
+#[component]
+fn ControlledInput() -> impl IntoView {
+    let (name, set_name) = create_signal("Controlled".to_string());
+
+    view! {
+        <input
+            type="text"
+            on:input=move |ev| {
+                set_name.set(event_target_value(&ev));
+            }
+        />
+
+        // the `prop:` syntax lets you update a DOM property,
+        // rather than an attribute.
+        // prop:value=name
+        // value=name // this would only initial value
+        <p>"Name is: " {name}</p>
+    }
+}
+
+// Uncontrolled Input
+#[component]
+fn UncontrolledInput() -> impl IntoView {
+    let (name, set_name) = create_signal("Uncontrolled".to_string());
+
+    // NodeRef is a kind of reactive smart pointer: we can use it to access the
+    // the underlying DOM node.
+    let input_element: NodeRef<html::Input> = create_node_ref();
+
+    let on_submit = move |ev: leptos::ev::SubmitEvent| {
+        // stop the page from reloading
+        ev.prevent_default();
+
+        // here, we'll extract the value from the input
+        let value = input_element
+            .get()
+            // event handlers can only fire after the view
+            // is mounted to the DOM, so the `NodeRef` will be `Some`
+            .expect("<input> should be mounted")
+            // `leptos::HtmlElement<html::Input>` implement `Deref`
+            // to a `web_sys::HtmlInputElement`.
+            // this means we can call `HtmlInputElement::value()`
+            // to get the current value of the input
+            .value();
+        set_name.set(value);
+    };
+
+    view! {
+        <form on:submit=on_submit>
+            <input type="text" value=name node_ref=input_element/>
+            <input type="submit" value="Submit"/>
+        </form>
+        <p>"Name is: " {name}</p>
+    }
+}
+
+// Special Cases: <textarea> and <select>
+
+// <textarea> does not support a `value` attribute. Instead,
+// it receives its value as a plain text node in its HTML children.
+// But it does have value property:D
+#[component]
+fn TextArea() -> impl IntoView {
+    let (article, set_article) = create_signal("Type Something!".to_string());
+    view! {
+        <textarea
+            on:input=move |ev| {
+                set_article.set(event_target_value(&ev));
+            }
+
+            prop:value=article
+        ></textarea>
+        <p>"The article is: " {article}</p>
+    }
+}
+
+// <select> also does not have a `value` attribute, nor a `value` property.
+// Instead, its value is determined by the `selected` attribute of its
+// <option> field.
+
+#[component]
+fn Select() -> impl IntoView {
+    let (value, set_value) = create_signal("B".to_string());
+    view! {
+        <select on:change=move |ev| {
+            let new_value = event_target_value(&ev);
+            set_value.set(new_value);
+        }>
+
+            <option value="A" selected=move || value.get() == "A">
+                "A"
+            </option>
+            <option value="B" selected=move || value.get() == "B">
+                "B"
+            </option>
+        </select>
+        <p>"The selected is: " {value}</p>
+    }
+}
+
+// if there are so many option, we can refactored this
+#[component]
+fn SelectRefactored() -> impl IntoView {
+    let (value, set_value) = create_signal("B".to_string());
+    view! {
+        <select on:change=move |ev| {
+            let new_value = event_target_value(&ev);
+            set_value.set(new_value);
+        }>
+            // for now, the single value would expand to value=value
+            // which is not consist with html,
+            // in html, the single value would refer to value=true
+            // this may change in the future.
+            <SelectOption value is="A"/>
+            <SelectOption value is="B"/>
+            <SelectOption value is="C"/>
+        </select>
+        <p>"The selected is: " {value}</p>
+    }
+}
+
+#[component]
+pub fn SelectOption(is: &'static str, value: ReadSignal<String>) -> impl IntoView {
+    view! {
+        <option value=is selected=move || value.get() == is>
+            {is}
+        </option>
+    }
+}
+
+// Control Flow
+
+#[component]
+fn ControlFlow() -> impl IntoView {
+    let (value, set_value) = create_signal(0);
+    let is_odd = move || value.get() & 1 == 1;
+    let message1 = move || {
+        if is_odd() {
+            Some("Ding Ding Ding")
+        } else {
+            None
+        }
+    };
+    // the above can make is shorter
+    let message2 = move || is_odd().then(|| "Ding Ding Ding");
+    let message3 = move || match value.get() {
+        0 => "Zero",
+        1 => "One",
+        _ if is_odd() => "Odd",
+        _ => "Even",
+    };
+    view! {
+        <button on:click=move |_| {
+            set_value.update(|v| *v += 1)
+        }>
+
+            "Add 1"
+        </button>
+        <p>
+            "because `&str` implements IntoView, so" <pre>"Fn() -> &str"</pre> "would reactive!"
+            <br/> "It's just work www"
+        </p>
+        <p>
+
+            // to prevent expensive over re-rendering, we can use
+            // <Show/> Instead of move || if
+            {move || if is_odd() { "Odd" } else { "Even" }}
+
+        </p>
+        <p>{message1}</p>
+        <p>{message2}</p>
+        <p>{message3}</p>
+    }
+}
+
+#[component]
+fn TypeConversions() -> impl IntoView {
+    let (value, set_value) = create_signal(0);
+    let is_odd = move || value.get() & 1 == 1;
+    view! {
+        <button on:click=move |_| {
+            set_value.update(|v| *v += 1)
+        }>
+
+            "Add 1"
+        </button>
+        {move || match is_odd() {
+            true if value.get() == 1 => view! { <pre>"One"</pre> }.into_any(),
+            false if value.get() == 2 => view! { <p>"Two"</p> }.into_any(),
+            _ => view! { <textarea>{value.get()}</textarea> }.into_any(),
+        }}
+    }
+}
+
+// Error Handling
+
+#[component]
+fn NumericInput() -> impl IntoView {
+    let (value, set_value) = create_signal(Ok(0));
+
+    // when input changes, try to parse a number from the input
+    let on_input = move |ev| set_value.set(event_target_value(&ev).parse::<i32>());
+
+    // without error handling, this would show nothing if the parser error happen.
+    view! {
+        <label>
+            "Type a number (or not!)" <input on:input=on_input/>
+            <p>"You entered " <strong>{value}</strong></p>
+        </label>
+    }
+}
+
+// handle error using <ErrorBoundary/>
+
+#[component]
+fn ErrorHandleNumericInput() -> impl IntoView {
+    let (value, set_value) = create_signal(Ok(0));
+
+    // when input changes, try to parse a number from the input
+    let on_input = move |ev| set_value.set(event_target_value(&ev).parse::<i32>());
+
+    // without error handling, this would show nothing if the parser error happen.
+    view! {
+        <label>
+            "Type a number (or not!)" <input on:input=on_input/>
+            // the fallback receives a signal containing current errors
+            <ErrorBoundary fallback=|errs| {
+                view! {
+                    <div class="error">
+                        <p>"Not a number! Errors: "</p>
+                        // we can render a list of errors as strings, if we'd like
+                        <ul>
+                            {move || {
+                                errs.get()
+                                    .into_iter()
+                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                                    .collect_view()
+                            }}
+
+                        </ul>
+                    </div>
+                }
+            }>
+
+                <p>"You entered " <strong>{value}</strong></p>
+            </ErrorBoundary>
+        </label>
+    }
+}
