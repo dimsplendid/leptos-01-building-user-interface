654e23409354d62049d2484720d39fbe6458cc5e
dimsplendid <dimsplendid@gmail.com>
2024-06-16 22:10:08 +0800

    iter finish

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,10 @@ fn App() -> impl IntoView {
         <div>
             <DynamicList length=5/>
         </div>
+        <div>
+            <p>Complex List</p>
+            <ComplexList/>
+        </div>
     }
 }
 
@@ -206,6 +210,60 @@ fn DynamicList(
                 />
 
             </ul>
+
         </div>
     }
 }
+
+#[derive(Debug, Clone)]
+struct DatabaseEntry {
+    key: String,
+    value: i32,
+}
+
+#[component]
+fn ComplexList() -> impl IntoView {
+    // start with as set of three rows
+    let (data, set_data) = create_signal(vec![
+        DatabaseEntry {
+            key: "foo".to_string(),
+            value: 10,
+        },
+        DatabaseEntry {
+            key: "bar".to_string(),
+            value: 20,
+        },
+        DatabaseEntry {
+            key: "baz".to_string(),
+            value: 30,
+        }
+    ]);
+    view! {
+        // when we click, update each row,
+        // doubling its value
+        <button on:click=move |_| {
+            set_data.update(|data| {
+                for row in data {
+                    row.value *= 2;
+                }
+            });
+            // log the new value of the signal
+            logging::log!("{:?}", data.get());
+        }>
+            "Update Values"
+        </button>
+        // iterate over the rows and display each value
+        <For
+            each=move || data.get()
+            key=|state| state.key.clone()
+            let:child
+        >
+            <p>{child.value}</p>
+        </For>
+    }
+}
+
+// Forms and Inputs
+
+// Controlled Inputs
+// Framework controls the state of the input element
